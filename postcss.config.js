16a2d7a512cb4f85254a8b2232358f73e376b1eb
Aerethos <87134976+Aerethos@users.noreply.github.com>
2026-03-13 23:35:31 +0000

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+---
+
+### **File 6: .gitignore**
+Save as: `.gitignore`
+```
+node_modules
+.next
+out
+.DS_Store
+*.log
+.vercel
